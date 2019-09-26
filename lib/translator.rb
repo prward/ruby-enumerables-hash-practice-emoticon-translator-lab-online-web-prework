262c14e52f0d7325d01656d858f11a8a192769c5
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-26 15:11:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,10 @@ def load_library(path)
   emoticon_hash("get_emoticon") = Hash.new
   
   emoticons.each do |english_word, emoticon_set|
-    
+     emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
+    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
+  end
+  emoticon_hash
   
 end
 
