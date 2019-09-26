4967d789e0b11ed7322db44341f8016572baadd5
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-26 15:09:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,12 @@
 # require modules here
 require "yaml"
+
 def load_library(path)
   emoticons = YAML.load_file(path)
   emoticon_hash = Hash.new 
-  emoticon_hash("Get_meaning") = Hash.new
+  emoticon_hash("get_meaning") = Hash.new
+  emoticon_hash("get_emoticon") = Hash.new
+  
 end
 
 def get_japanese_emoticon
